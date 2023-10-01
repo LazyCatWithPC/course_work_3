1eb74a522c5b6df69eee9999d36b753b98c5c3c4
LazyCatWithPC <i.treukhov@gmail.com>
2023-10-01 14:35:22 +0400

    added get_last_values in utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from json import load
+from datetime import datetime
 
 
 def get_data():
@@ -9,6 +10,12 @@ def get_data():
 
 def filter_data(data):
     data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
+    return data
+
+
+def get_last_values(data, count_last_values):
+    data = sorted(data, key=lambda x: x['date'], reverse=True)
+    data = data[:count_last_values]
     return data
 
 
