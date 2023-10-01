e79be6581e108aebfe9eade294427213c41333a6
LazyCatWithPC <i.treukhov@gmail.com>
2023-10-01 15:37:54 +0400

    added get_formatted_data in utils.py changed get_last_values in utils.py

--- utils.py
+++ utils.py
@@ -13,10 +13,24 @@ def filter_data(data):
     return data
 
 
-def get_last_values(data, count_last_values):
-    data = sorted(data, key=lambda x: x['date'], reverse=True)
-    data = data[:count_last_values]
+def get_last_values(data):
+    data = sorted(data, key=lambda x: x['date'], reverse=True)[:5]
     return data
 
 
+def get_formatted_data(data):
+    formatted_data = []
+    for row in data:
+        date = datetime.strptime(row['date'], __format="%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        description = data["description"]
+        from_info = ''
+        if 'from' in row:
+            from_info = data['from'] + ' ->'
+        recipient = data['to']
+        operations_amount = f"{data['operationAmount']['amount']} {data['operationAmount']['currency']['name']}"
 
+        formatted_data.append(f"""\
+{date} {description}
+{from_info} {recipient}
+{operations_amount}""")
+    return formatted_data
